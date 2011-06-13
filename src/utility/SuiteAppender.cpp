464d2c60f21ea9b9767514f55228f4dd51626167
matthew.murdoch.0@gmail.com <matthew.murdoch.0@gmail.com@a70d49be-46d1-11de-80ae-c1ff141340a0>
2011-06-13 20:08:46 +0000

    Updated copyright dates.
    git-svn-id: http://arduinounit.googlecode.com/svn/trunk@74 a70d49be-46d1-11de-80ae-c1ff141340a0

--- src/utility/SuiteAppender.cpp
+++ src/utility/SuiteAppender.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009 Matthew Murdoch
+Copyright (c) 2009-2011 Matthew Murdoch
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
